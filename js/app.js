087f696e9e1195b9ecd5477f84503d855298e250
Alex Soper <alexsoper29@gmail.com>
2014-11-14 12:19:15 -0500

    Added enter function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
-(function( window ) {
-	'use strict';
+// Your starting point. Enjoy the ride!
 
-	// Your starting point. Enjoy the ride!
+// $("#/active").click(function(){
+// 	$( ".main > li" ).not( ".completed" ).css( "display", "none" );
+// })
 
-})( window );
+var $input = $('#new-todo');
+
+$input.keyup(function(enter){
+	if(enter.keyCode === 13) {
+		$(this).trigger('addTask');
+	}
+})
+
+$input.bind('addTask', function(){
+	if ($input.val().trim().length > 0) {
+		$('#todo-list').show().append('<li><label>' + $input.val() + '</label></li>');
+		$('#main').css({display: 'block'});
+		$input.val('');
+	}
+});
