0e68b5e1c465ef0b17da80004bf69c6fc724b81b
Nicholas Hare <oharemarketing@gmail.com>
2014-11-14 15:12:55 -0500

    bday tinkering

--- js/app.js
+++ js/app.js
@@ -1,6 +1,66 @@
 (function( window ) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+//Input
+	var $input = $('#new-todo');
+
+$input.keyup(function(enter){
+	if(enter.keyCode === 13) {
+		$(this).trigger('addTask');
+	}
+});
+
+$input.bind('addTask', function(){
+	if ($input.val().trim().length > 0) {
+		$('#todo-list').show().append('<li><div class="view"><input class="toggle" type="checkbox"><label>' + $input.val() + '</label><button class="destroy"></button></input></div></li>');
+		$('#main').css({display: 'block'});
+		$input.val('');
+	}
+});
+
+
+
+$('#todo-list').on('click', '.destroy', function(){
+	$(this).closest('li').remove();
+});
+
+
+$('#todo-list').on('click', '.toggle', function(){
+	if($(this).closest('li').hasClass('completed')){
+		$(this).closest('li').removeClass('completed');
+	} else {
+		$(this).closest('li').addClass('completed');
+	}
+});
+
+$('#main').on('click', '#toggle-all', function(){
+	var $todo = $('todo-list > li');
+	console.log('hi');
+	$($todo).each(function(){
+		if($(todo).closest('li').hasClass('completed')){
+			$(todo).closest('li').removeClass('completed');
+		} else {
+			$(todo).closest('li').addClass('completed');
+		}
+	});
+	// return false;
+});
+
+
+$("#filters").click(function() {
+	$('.active, .completed').show();
+});
+
+$('#filters .active').click(function() {
+	$('.active').show();
+	$('.completed').hide();
+	return false;
+});
+
+$('#filters .completed').click(function() {
+	$('.completed').show();
+	$('.active').hide();
+	return false;
+});
 
 })( window );
