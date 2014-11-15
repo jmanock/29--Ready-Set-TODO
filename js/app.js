5f3f18587e4719778266c5361f256fcfaa970c42
Alex Soper <alexsoper29@gmail.com>
2014-11-15 15:59:53 -0500

    Added todo counter and fixd edit function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+//Keep count of incompleted tasks
+function todoCounter(){
+	var todoCount = ($('#todo-list').children('li').length) - ($('#todo-list').children('.completed').length);
+	$('#todo-count strong').html(todoCount);
+	//Display footer only if tasks are in the list
+	if ($('#todo-list').children('li').length === 0){
+		$('#footer').css('display', 'none');
+	} else {
+		$('#footer').css('display', 'block');
+	}
+}
+
+//Call todoCounter on startup
+todoCounter();
+
 //Entering a new LI into the list when you hit Enter
 var $input = $('#new-todo');
 
@@ -7,6 +22,7 @@ $input.keyup(function(enter){
 			$('#todo-list').show().append('<li><div class="view"><input class="toggle" type="checkbox"><label>' + $input.val() + '</label><button class="destroy"></button></input></div><input class="edit" value="' + $input.val() + '"></input></li>');
 			$('#main').css({display: 'block'});
 			$input.val('');
+			todoCounter();
 		}
 	}
 });
@@ -14,23 +30,25 @@ $input.keyup(function(enter){
 //Delete functionality for each LI
 $('#todo-list').on('click', '.destroy', function(){
 	$(this).closest('li').remove();
+	todoCounter();
 });
 
 //Edit functionality for each LI
-$('#todo-list').on('dblclick','.view label', function(){
-	$(this).html($('.editing label').val());
-	$(this).closest('li').addClass('editing');
-
+$('#todo-list').on('dblclick','.view label', function(e){
+	$newInput = $(e.target).closest('li').addClass('editing').find('.edit');
+	$newInput.html($('.editing label').val());
+	$newInput.focus();
 	//Change the value of the LI being edited and remove the Editing class
 	$('#todo-list').on('keyup', '.edit', function(enter){
 		if(enter.keyCode === 13) {
-			$('.editing label').html($(this).val().trim());
+			$('.editing label').html($newInput.val().trim());
 			$('.editing').removeClass('editing');
 		}
 	});
 });
 
 //Remove the Editing class when you click anywhere in the document
-$(document).click(function (){
+$(document).click(function(){
+	$('.editing label').html($newInput.val().trim());
 	$('.editing').removeClass('editing');
 });
